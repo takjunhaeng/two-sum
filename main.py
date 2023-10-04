cf72e5c712aba3aadea9b1fdaa0cce77916bc933
takjunhaeng <tjh0206@gmail.com>
2023-10-04 10:36:44 +0000

    update main.py

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 from typing import List
-def twoSum(self, nums: List[int], target: int) -> List[int]:
-    # YOUR ANSWER
-    return
+def twoSum(nums: List[int], target: int) -> List[int]:
+    
+    print("Input: nums =", nums, ", target =", target)
+    
+    nums_length = len(nums)
+    for i in range(nums_length - 1):
+        for n in range(i+1, nums_length):
+            if nums[i] + nums[n] == target:
+                return [i, n]
+
+def main():
+    nums_input = input('Enter list: ')
+    nums = [int(x) for x in nums_input.split(',')]
+    
+    target = int(input('Enter target: '))
+    
+    result = twoSum(nums, target)
+    
+    print("Output:", result)
+    
+    
+main()
